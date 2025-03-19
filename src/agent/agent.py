12f45799b806aec6f1cee082b8b825a5a9657556
Hapliuc Cesar-Angel <127996980+hapliuc@users.noreply.github.com>
2025-03-19 14:26:36 +0200

    Added agent and tools

---
+++ src/agent/agent.py
@@ -0,0 +1,40 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+from llm_functions import llm_completion, llm_function_call, llm_load_tools
+
+def create_folder(path: str) -> str:
+    os.popen(f'mkdir {path}')
+    return f'Created directory {path}'
+
+def create_file(name: str) -> str:
+    os.popen(f'touch {name}')
+    return f'Created file {name}'
+
+def main() -> None:
+    load_dotenv()
+    tool_path = str(os.getenv('TOOL_PATH'))
+    tools: list = []
+    messages: list = []
+    function_list = [create_folder, create_file]
+
+    client = OpenAI(
+        base_url='http://localhost:11434/v1',
+        api_key='ollama'
+    )
+
+    llm_load_tools(tool_path, tools)
+    system_prompt: dict = {'role': 'system', 'content': 'You are a helpful AI chatbot with agentic capabilities. Use the tools you have acces to, to fulfill user requests'}
+    messages.append(system_prompt)
+    
+    user_input: str = input('> User Prompt: ')
+    user_prompt: dict = {'role': 'user', 'content': user_input}
+    messages.append(user_prompt)
+
+    completion_1 = llm_completion(client, 'llama3.2', messages, tools)
+    llm_function_call(completion_1, messages, function_list)
+    completion_2 = llm_completion(client, 'llama3.2', messages, tools)
+    print(f'> LLM Response:', completion_2.choices[0].message.content)
+
+if __name__ == '__main__':
+    main()
---
+++ src/agent/llm_functions.py
@@ -0,0 +1,81 @@
+import os
+import json
+import logging
+from json.decoder import JSONDecodeError
+from openai import OpenAI, APIConnectionError
+from openai.types.chat import ChatCompletion
+
+
+logger = logging.getLogger(__name__)
+
+
+def llm_completion(client: OpenAI, model: str, messages: list, tools: list) -> ChatCompletion: 
+    try:
+        return client.chat.completions.create(
+            model=model,
+            messages=messages,
+            tools=tools
+        )
+    except APIConnectionError as e:
+        logger.critical(f'Exception: {e.__class__.__name__} - {e}', exc_info=True)
+        exit(1)
+
+
+def llm_completion_structured(client: OpenAI, model: str, messages: list, tools: list, response_format):
+    try:
+        return client.beta.chat.completions.parse(
+            model=model,
+            messages=messages,
+            tools=tools,
+            response_format=response_format
+        )
+    except APIConnectionError as e:
+        logger.critical(f'Exception: {e.__class__.__name__} - {e}', exc_info=True)
+        exit(1)
+    except TypeError as e:
+        logger.critical(f'Exception: {e.__class__.__name__} - {e}', exc_info=True)
+        exit(1)
+
+
+def llm_load_tools(path: str, tools: list) -> None:
+    try:
+         for file in os.listdir(path):
+             if file.endswith('.json'):
+                 with open(f'{path}/{file}', 'r') as f:
+                     tools.append(json.loads(f.read()))
+
+    except JSONDecodeError as e:
+        logger.critical(f'Exception: {e.__class__.__name__} - {e}', exc_info=True)
+        exit(1)
+
+
+def llm_function_call(completion: ChatCompletion, messages: list, function_list: list) -> None:
+    try:
+        if completion.choices[0].message.tool_calls:
+            def call_function(name, args):
+                nonlocal function_list
+                for function in function_list:
+                    if name == function.__name__:
+                        return function(**args)
+
+            for tool_call in completion.choices[0].message.tool_calls:
+                name = tool_call.function.name
+                args = json.loads(tool_call.function.arguments)
+                result = call_function(name, args)
+                messages.append(completion.choices[0].message)
+                messages.append({
+                    'role': 'tool',
+                    'tool_call_id': tool_call.id,
+                    'content': result
+                })
+    except AttributeError as e:
+        logger.critical(f'Exception: {e.__class__.__name__} - {e}', exc_info=True)
+        exit(1)
+    
+
+def main() -> None:
+    logging.basicConfig(level=logging.DEBUG)
+
+if __name__ == '__main__':
+    main()
+
